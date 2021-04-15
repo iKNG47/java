a90335b06c3c3ce1a719d29ef1d09628c76ee3e7
iKNG47 <82219345+iKNG47@users.noreply.github.com>
2021-04-15 11:40:44 +0200

    Add files via upload

---
+++ calculator/Calculate.java
@@ -0,0 +1,13 @@
+package calculator;
+
+
+public class Calculate {
+
+    public static void main(String[] args) {
+        Calculator sum = new Calculator();
+        double a = 3;
+        double b = 2.4;
+
+        System.out.println(sum.subtract(a, b));
+    }
+}
---
+++ calculator/Calculator.java
@@ -0,0 +1,28 @@
+package calculator;
+
+public class Calculator {
+     static double add(double x, double y){
+        double sum;
+        sum=x+y;
+        return sum;
+    }
+
+    static double subtract(double x, double y){
+        double sub;
+        sub=x-y;
+        return sub;
+    }
+
+    static double multiply(double x, double y){
+        double mul;
+        mul=x*y;
+        return mul;
+    }
+
+    static double divide(double x, double y){
+        double div;
+        div=x/y;
+        return div;
+    }
+
+}
