f27850bda2aa35a9b082861c526d7a97a2c70352
iKNG47 <82219345+iKNG47@users.noreply.github.com>
2021-05-12 23:44:10 +0200

    Add files via upload

---
+++ progHomeWork7/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+        System.out.println("Wprowadz dowolny text:");
+        String inputUsersText = new String();
+        inputUsersText = scanner.nextLine();
+
+            char firstChar = inputUsersText.charAt(0);
+
+            if (Character.isUpperCase(firstChar)) {
+                System.out.println(inputUsersText.toUpperCase(Locale.ROOT));
+            } else if ((Character.isLowerCase(firstChar))) {
+                System.out.println(inputUsersText.toLowerCase(Locale.ROOT));
+            } else {
+                System.out.println(inputUsersText);
+            }
+        }
+    }
+}
